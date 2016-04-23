ba9edc8a894577bd2e7851325fb85ead81a4ef7a
ctmcquilkin <ctmcquilkin@gmail.com>
2016-04-23 17:37:36 -0500

    fixed country page name, fixed api url

--- scripts.js
+++ scripts.js
@@ -13,15 +13,16 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
         })
         .otherwise('/indexCtrl');
     }])
-    .factory('DataRequest', ['$http', '$q', function ($http, $q, GEO_NAMES_USER_NAME) {
+    .factory('allCountriesRequest', ['$http', '$q', function ($http, $q, GEO_NAMES_USER_NAME) {
     	return {
-    		getData : function (country) {
+    		getAllItems : function (country) {
     			// deferred object
     			var deferred = $q.defer();
-    			var apiUrl = 'http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=' + GEO_NAMES_USER_NAME;
+    			var apiUrl = 'http://api.geonames.org/countryInfoJSON?username=' + GEO_NAMES_USER_NAME;
     			
     			$http.defaults.useXDomain = true;
     			delete $http.defaults.headers.common['X-Requested-With'];
+    			console.log('hi');
     			
     			// Calling Web API
     			$http.get(apiUrl).success(function (data) {
@@ -39,25 +40,24 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
 	.controller('indexCtrl', function($scope) {
 		// empty for now
 	})
-	.controller('listCtrl', ['$scope', 'DataRequest', function ($scope, DataRequest) {
-// 		$scope.countries = [];
-// 		$scope.items = [];
-// 		
-// 		DataRequest.getData($scope.country).then(function (data) {
-// 				var parsedData = angular.fromJson(data);
-// 				$scope.items = parsedData.geonames;
-// 				alert(data.message);
-// 			
-// 				// loop through geonames data
-// 				for (var i = 0; i < $scope.items.length; i++) {
-// 					var country = $scope.items[i];
-// 					$scope.countries.push({ /* write code here to parse each country */ });
-// 				}
-// 			},
-// 			function (errorMessage) {
-// 				$scope.error = errorMessage;
-// 			});	
-// 		};
+    .controller('listCtrl', ['$scope', 'allCountriesRequest', function ($scope, allCountriesRequest) {
+        $scope.countries = [];
+        $scope.items = [];
+
+        allCountriesRequest.getAllItems($scope.country).then(function (data) {
+            var parsedData = angular.fromJson(data);
+            $scope.items = parsedData.geonames;
+            alert(data.message);
+
+			// loop through geonames data
+            for (var i = 0; i < $scope.items.length; i++) {
+                var country = $scope.items[i];
+                $scope.countries.push({ /* write code here to parse each country */ });
+            }
+        },
+        function (errorMessage) {
+            $scope.error = errorMessage;
+        });
 	}])
 	.controller('detailsCtrl', function($scope) {
 		// empty for now
