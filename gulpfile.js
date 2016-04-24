515b9e2c8c4001b9f0dff8901fa1859563e6a118
Chuck McQuilkin <ctmcquilkin@gmail.com>
2016-04-23 23:33:45 -0500

    added gulp tasks (but they don't do much yet

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+
+gulp.task('copy-html-files', function() {
+    gulp.src(['app/*.html', '!app/index.html'], {base: 'app'})
+      .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', function() {
+    gulp.src('app/index.html')
+        .pipe(usemin({
+                css: [minifyCSS(), 'concat'],
+                      js: [uglify()]
+                          }))
+                              .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['copy-html-files', 'usemin']);
