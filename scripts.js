13cc5763f0540e3010010a3bc39a5da52792b3f3
Chuck McQuilkin <ctmcquilkin@gmail.com>
2016-04-23 21:55:41 -0500

    querying API, still trying to figure out how to display the output

--- scripts.js
+++ scripts.js
@@ -13,30 +13,6 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
         })
         .otherwise('/indexCtrl');
     }])
-    .factory('allCountriesRequest', ['$http', '$q', function ($http, $q, GEO_NAMES_USER_NAME) {
-    	return {
-    		getAllItems : function (country) {
-    			// deferred object
-    			var deferred = $q.defer();
-    			var apiUrl = 'http://api.geonames.org/countryInfoJSON?username=' + GEO_NAMES_USER_NAME;
-    			
-    			$http.defaults.useXDomain = true;
-    			delete $http.defaults.headers.common['X-Requested-With'];
-    			console.log('hi');
-    			
-    			// Calling Web API
-    			$http.get(apiUrl).success(function (data) {
-    				// Passing data to deferred's resolve function on successful completion
-    				deferred.resolve(data);
-    			}).error(function (error) {
-    				// Sending a friendly error message in case of failure
-    				deferred.reject('An error occurred while fetching items.');
-    			});
-    			// Returning the promise object
-    			return deferred.promise;
-    		}
-    	}
-    }])
 	.controller('indexCtrl', function($scope) {
 		// empty for now
 	})
@@ -44,16 +20,17 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
         $scope.countries = [];
         $scope.items = [];
 
-        allCountriesRequest.getAllItems($scope.country).then(function (data) {
+        allCountriesRequest.getAllItems().then(function (data) {
             var parsedData = angular.fromJson(data);
-            $scope.items = parsedData.geonames;
-            alert(data.message);
+            $scope.items = parsedData;
+            alert($scope.items);
 
 			// loop through geonames data
             for (var i = 0; i < $scope.items.length; i++) {
                 var country = $scope.items[i];
-                $scope.countries.push({ /* write code here to parse each country */ });
+                $scope.countries.push({ continent: item.continent, capital: item.capital, population: item.population, area: item.areaInSqKm, code: item.countryCode, name: item.countryName });
             }
+            // alert($scope.items);
         },
         function (errorMessage) {
             $scope.error = errorMessage;
@@ -61,4 +38,27 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
 	}])
 	.controller('detailsCtrl', function($scope) {
 		// empty for now
-	});
+	})
+    .factory('allCountriesRequest', ['$http', '$q', function ($http, $q, GEO_NAMES_USER_NAME) {
+        return {
+            getAllItems : function () {
+                // deferred object
+                var deferred = $q.defer();
+                var apiUrl = 'http://api.geonames.org/countryInfoJSON?username=' + GEO_NAMES_USER_NAME;
+                
+                $http.defaults.useXDomain = true;
+                delete $http.defaults.headers.common['X-Requested-With'];
+                
+                // Calling Web API
+                $http.get(apiUrl).success(function (data) {
+                    // Passing data to deferred's resolve function on successful completion
+                    deferred.resolve(data);
+                }).error(function (error) {
+                    // Sending a friendly error message in case of failure
+                    deferred.reject('An error occurred while fetching items.');
+                });
+                // Returning the promise object
+                return deferred.promise;
+            }
+        }
+    }]);
