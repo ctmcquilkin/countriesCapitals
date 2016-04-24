af6b46a1376d463b4f6085300b664a77a53dc0a2
Chuck McQuilkin <ctmcquilkin@gmail.com>
2016-04-24 15:48:19 -0500

    displaying parsed json from geonames.org in a table

--- app/scripts.js
+++ app/scripts.js
@@ -13,52 +13,33 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
         })
         .otherwise('/indexCtrl');
     }])
+    .factory('allCountriesRequest', function($http) {
+       return {
+         getData: function() {
+           return $http.get('http://api.geonames.org/countryInfoJSON?username=pixl_pshr').then(function(result) {
+               return result.data;
+           });
+         }
+       }
+    })
 	.controller('indexCtrl', function($scope) {
 		// empty for now
 	})
     .controller('listCtrl', ['$scope', 'allCountriesRequest', function ($scope, allCountriesRequest) {
+
         $scope.countries = [];
-        $scope.items = [];
 
-        allCountriesRequest.getAllItems().then(function (data) {
-            var parsedData = angular.fromJson(data);
-            $scope.items = parsedData;
-            alert($scope.items);
+       allCountriesRequest.getData().then(function(data) {
+           // $scope.countries = data;
+           var parsedData = angular.fromJson(data);
+           $scope.countries = parsedData.geonames;
+           console.log(parsedData);
 
-			// loop through geonames data
-            for (var i = 0; i < $scope.items.length; i++) {
-                var country = $scope.items[i];
-                $scope.countries.push({ continent: item.continent, capital: item.capital, population: item.population, area: item.areaInSqKm, code: item.countryCode, name: item.countryName });
-            }
-            // alert($scope.items);
-        },
-        function (errorMessage) {
-            $scope.error = errorMessage;
-        });
+           // for (var i = 0; i < $scope.countries.length; i++) {
+           //  var 
+           // }
+       });
 	}])
 	.controller('detailsCtrl', function($scope) {
 		// empty for now
-	})
-    .factory('allCountriesRequest', ['$http', '$q', function ($http, $q, GEO_NAMES_USER_NAME) {
-        return {
-            getAllItems : function () {
-                // deferred object
-                var deferred = $q.defer();
-                var apiUrl = 'http://api.geonames.org/countryInfoJSON?username=' + GEO_NAMES_USER_NAME;
-                
-                $http.defaults.useXDomain = true;
-                delete $http.defaults.headers.common['X-Requested-With'];
-                
-                // Calling Web API
-                $http.get(apiUrl).success(function (data) {
-                    // Passing data to deferred's resolve function on successful completion
-                    deferred.resolve(data);
-                }).error(function (error) {
-                    // Sending a friendly error message in case of failure
-                    deferred.reject('An error occurred while fetching items.');
-                });
-                // Returning the promise object
-                return deferred.promise;
-            }
-        }
-    }]);
+	});
