d44509d6c252d38ff1dd8d077090f12aa3a8c6c9
ctmcquilkin <ctmcquilkin@gmail.com>
2016-05-12 17:43:58 -0500

    styled the app, added search filter, pagination, show all button, sorting by name, capital, etc. toggle list view is still buggy, added styling to country detail page, styled buttons

--- app/scripts.js
+++ app/scripts.js
@@ -1,8 +1,8 @@
-var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
+var CountriesCapitals = angular.module("root", ['ui.bootstrap', 'ngAnimate', 'ngRoute'])
 	.constant('GEO_NAMES_USER_NAME', 'pixl_pshr')
     .config(['$routeProvider', function($routeProvider){
         $routeProvider.when('/', {
-            template : '<p>Welcome to Countries & Capitals</p>',
+            template : "<p>Pocket Atlas is an Angular App that displays data from GeoNames.org.</p><a class='cc-btn' href='#/countries'>Explore</a>",
             controller : 'indexCtrl'
         }).when('/countries', {
           templateUrl : 'countries.html',
@@ -77,6 +77,94 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
       }
 		};
 	})
+	.factory('sortable', ['$filter', '$rootScope', function($filter, $rootScope){
+
+	  return function (scope, data, itemsPerPage, initSortingOrder) {
+
+	
+		scope.sortingOrder = initSortingOrder;
+		scope.reverse = false;
+		scope.filteredItems = [];
+		scope.groupedItems = [];
+		scope.itemsPerPage = itemsPerPage;
+		scope.pagedItems = [];
+		scope.currentPage = 1;
+		scope.items = data;
+		scope.maxSize = 5;
+
+		  // init the filtered items
+		  scope.search = function () {
+
+			scope.filteredItems = $filter('filter')(scope.items, $rootScope.query);
+			console.log(scope.filteredItems);
+
+			  // take care of the sorting order
+			if (scope.sortingOrder !== '') {
+				scope.filteredItems = $filter('orderBy')(scope.filteredItems, scope.sortingOrder, scope.reverse);
+			}
+
+			  scope.currentPage = 1;
+
+			  // now group by pages
+			  scope.groupToPages();
+
+			  // reset the total items for the pagination buttons
+			  scope.totalItems = scope.filteredItems.length;
+		  };
+	  
+
+		  // calculate page in place
+		  scope.groupToPages = function () {
+			  scope.pagedItems = [];
+		  
+			  for (var i = 0; i < scope.filteredItems.length; i++) {
+				  if (i % scope.itemsPerPage === 0) {
+					  scope.pagedItems[Math.floor(i / scope.itemsPerPage)] = [ scope.filteredItems[i] ];
+				  } else {
+					  scope.pagedItems[Math.floor(i / scope.itemsPerPage)].push(scope.filteredItems[i]);
+				  }
+			  }
+		  };
+	  
+
+		  scope.range = function (start, end) {
+			  var ret = [];
+			  if (!end) {
+				  end = start;
+				  start = 0;
+			  }
+			  for (var i = start; i < end; i++) {
+				  ret.push(i);
+			  }
+			  return ret;
+		  };
+	  
+		  scope.showAll = function () {
+			scope.itemsPerPage = scope.totalItems;
+			scope.groupToPages();
+		  };
+
+
+		  // functions have been described process the data for display
+		  scope.search();
+
+
+		  // change sorting order
+		  scope.sort_by = function(newSortingOrder) {
+			  if (scope.sortingOrder == newSortingOrder)
+				  scope.reverse = !scope.reverse;
+
+			  scope.sortingOrder = newSortingOrder;
+
+			  // call search again to make sort affect whole query
+			  scope.search();
+		  };
+
+		  scope.sort_by(initSortingOrder);
+		  scope.totalItems = scope.filteredItems.length;
+	  }
+
+	}])
   .filter('capitalize', function() {
     return function(input, scope) {
       if (input!=null)
@@ -84,6 +172,12 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
       return input.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase() });
     }
   })
+	.filter('startFrom', function() {
+		return function(input, start) {
+			start = +start; //parse to int
+			return input.slice(start);
+		}
+	})
   .filter('joinBy', function () {
         return function (input,delimiter) {
             return (input || []).join(delimiter || ',');
@@ -98,13 +192,39 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
 		// empty for now
 	})
 
-  .controller('listCtrl', ['$scope', '$window', 'allCountriesRequest', '$routeParams', 'countryService', 'countryRequest', function ($scope, $window, allCountriesRequest, $routeParams, countryService, countryRequest) {
+  .controller('listCtrl', ['$rootScope', '$scope', '$window', 'allCountriesRequest', '$routeParams', 'countryService', 'countryRequest', 'sortable', '$filter', function ($rootScope, $scope, $window, allCountriesRequest, $routeParams, countryService, countryRequest, sortable, $filter) {
 
       $scope.params = $routeParams;
+
+		$rootScope.query = '';
+	
+		$scope.gridToggleList = true;
+		
+		console.log($scope.gridToggleList);
+		$scope.action = function(){
+			if ($scope.gridToggleList) {
+				$scope.gridToggleThumb = true;
+				$scope.gridToggleList = false;
+				console.log($scope.gridToggleList);
+			} else {
+				$scope.gridToggleThumb = false;
+				$scope.gridToggleList = true;
+				console.log($scope.gridToggleList);
+			}
+
+		};	
+
+		$scope.onQueryChange = function(val){
+		   $rootScope.query = val;
+		   $scope.search();
+		};
+
 
        allCountriesRequest.getData().then(function(data) {
            var parsedData = angular.fromJson(data);
            $scope.countries = parsedData.geonames;
+           sortable($scope, $scope.countries, 6, 'countryName');
+			
        });
 
       $scope.displayCountry = function() {
@@ -123,6 +243,7 @@ var CountriesCapitals = angular.module("root", ['ngAnimate', 'ngRoute'])
         $scope.countryData = countryService.getData();
         $scope.currentSelectedCountry = countryService.selected; 
         var countryCode = countryService.selected.countryCode;
+        $scope.mapBg = 'http://www.geonames.org/flags/x/' + countryCode + '.png) center center no-repeat';
 
         $scope.capitalFilter = function(geonames) {
           return geonames.fcodeName === "capital of a political entity";
